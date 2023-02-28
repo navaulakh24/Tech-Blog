bd842034f9f7a15adbb39e741de3cd380040843a
Nav <navdeep_aulakh24@hotmail.com>
2023-02-28 16:57:45 -0500

    login and logout post routes done

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -47,22 +47,6 @@ router.get("/:id", (req, res) => {
         });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 router.post("/", (req, res) => {
     User.create({
         username: req.body.username,
@@ -80,4 +64,38 @@ router.post("/", (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user with that username!" });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect password!" });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json({ user: dbUserData, message: "You are now logged in!" });
+        });
+    });
+});
+
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
