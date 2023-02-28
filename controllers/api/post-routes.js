f71ab468f7753a522cb7fa75c7a58dcf37ed5a81
Nav <navdeep_aulakh24@hotmail.com>
2023-02-28 16:23:23 -0500

    completed put route for :id in post-routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -49,3 +49,23 @@ router.get("/post/:id", async (req, res) => {
     }
 });
 
+router.put("/post/:id", withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update(
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id!" });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete("/post/:id", withAuth, async (req, res) => {
