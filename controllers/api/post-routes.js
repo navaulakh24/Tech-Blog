9e4029b318aee6e9ef4e313aa89a52f5a30c9393
Nav <navdeep_aulakh24@hotmail.com>
2023-02-28 16:18:18 -0500

    working in post-routes, completed router.get /

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
-const { User, Post, Comment } = require("../../models/");
+const { Post, Comment, User } = require("../../models/");
 const withAuth = require("../../utils/auth");
+
+
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render("homepage", {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/post/:id", async (req, res) => {
