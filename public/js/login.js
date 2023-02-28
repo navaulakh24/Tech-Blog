17a8a00523d6d839ecfeb98fa1b570e0d6b936a4
Nav <navdeep_aulakh24@hotmail.com>
2023-02-28 17:21:00 -0500

    completed login.js

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert("Unable to login. Please check your email and password and try again.");
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert("Unable to signup. Please check criteria and try again.");
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
