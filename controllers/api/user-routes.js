0b5cba7e326431aeb19dc10d564128ed2c47d435
Nav <navdeep_aulakh24@hotmail.com>
2023-02-28 16:43:22 -0500

    router.get completed to get all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+
 router.get("/", (req, res) => {
+User.findAll({
+attributes: { exclude: ["password"] }
+})
+.then(dbUserData => res.json(dbUserData))
+.catch(err => {
+console.log(err);
+res.status(500).json(err);
+});
+});
+
+router.get("/:id", (req, res) => {
+User.findOne({
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post("/", (req, res) => {
 User.create({
 username: req.body.username,
 password: req.body.password,
